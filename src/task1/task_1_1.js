52252b648270d6a8ef8ae8a0da9dd8cf621c7f40
Vlad <vladpotapow@yandex.ru>
2023-04-01 18:09:50 +0300

    Streams and file system

---
+++ src/task1/task_1_1.js
@@ -0,0 +1,8 @@
+// import readline from 'readline';
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on('line', (input) => console.log(input.split('').reverse().join('')));
---
+++ src/task1/task_1_2.js
@@ -0,0 +1,16 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const rStream = fs.createReadStream('src/task1/task_1_2.csv');
+const wStream = fs.createWriteStream('src/task1/task_1_2.txt');
+
+rStream
+  .pipe(
+    csv({
+      headers: ['book', 'author', 'amount', 'price'],
+      checkType: true,
+    })
+  )
+  .on('error', (error) => console.log(error))
+  .pipe(wStream)
+  .on('error', (error) => console.log(error));
